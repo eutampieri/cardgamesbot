cfb3d83cab17d6191b7a3e62620b28cda79a79d1
Eugenio Tampieri <eugenio@eutampieri.eu>
2020-12-30 19:18:29 +0100

    Enable Scala40

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,13 +1,13 @@
-mod primitives;
-mod utils;
-mod telegram;
-mod threading;
 mod bot;
 mod game_agent;
+mod primitives;
+mod telegram;
+mod threading;
+mod utils;
 
 use cardgames::primitives::Game;
-use std::sync::mpsc;
 use std::collections::HashMap;
+use std::sync::mpsc;
 
 // A game can last up to 10 minutes since the last action
 static MAX_GAME_DURATION: u64 = 600;
@@ -16,15 +16,25 @@ fn main() {
     // Data storage
     // Association between players and their respective games
     let mut player_games: HashMap<telegram_bot_raw::types::refs::UserId, String> = HashMap::new();
-    let mut game_channel: HashMap<String, std::sync::mpsc::SyncSender<threading::ThreadMessage>> = HashMap::new();
+    let mut game_channel: HashMap<String, std::sync::mpsc::SyncSender<threading::ThreadMessage>> =
+        HashMap::new();
     let mut game_last_played: HashMap<String, std::time::Instant> = HashMap::new();
-    
+
     let mut playable_games: Vec<Box<dyn Game>> = Vec::new();
     // List of playable games
     playable_games.push(Box::from(cardgames::games::briscola::Briscola::default()));
-    playable_games.push(Box::from(cardgames::games::beccaccino::Beccaccino::default()));
+    playable_games.push(Box::from(
+        cardgames::games::beccaccino::Beccaccino::default(),
+    ));
+    playable_games.push(Box::from(cardgames::games::scala40::Scala40::default()));
 
     println!("Starting CardGamesBot...");
     let mut client = telegram::Telegram::init();
-    bot::main_bot_logic(&playable_games, &mut player_games, &mut game_channel, &mut game_last_played, &mut client)
+    bot::main_bot_logic(
+        &playable_games,
+        &mut player_games,
+        &mut game_channel,
+        &mut game_last_played,
+        &mut client,
+    )
 }
